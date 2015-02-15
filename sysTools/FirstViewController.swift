ba389c275bed0eb022d44a99d29b32b2763b53e9
Kong XiangBo <yarshure@gmail.com>
2015-02-15 13:21:10 +0800

    Initial Commit

---
+++ sysTools/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  sysTools
+//
+//  Created by 孔祥波 on 15/2/15.
+//  Copyright (c) 2015年 Kong XiangBo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
